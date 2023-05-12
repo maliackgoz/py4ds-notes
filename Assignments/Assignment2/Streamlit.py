ffd97f5b9e05f50798f35d8cc87cce8805eb744a
maliackgoz <98320395+maliackgoz@users.noreply.github.com>
2023-05-13 00:57:29 +0300

    Add paragraphs.txt

--- Assignments/Assignment2/Streamlit.py
+++ Assignments/Assignment2/Streamlit.py
@@ -22,21 +22,23 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-paragraph_intro = "The use of Big Data in the video game industry has become increasingly important in recent years. With the rise of digital distribution and online gaming, companies have access to vast amounts of data on player behavior and preferences. By analyzing this data, companies can gain insights into player engagement, identify trends, and develop targeted marketing strategies. Additionally, data analysis can be used to inform game design decisions, leading to more engaging and successful games. This has resulted in a shift towards a data-driven approach to game development and marketing. In this context, the use of Big Data has the potential to revolutionize the video game industry and shape the future of gaming."
+file_path = "Assignments/Assignment2/paragraphs.txt"  # Replace with the actual file path
 
-paragraph1 = "I had trouble finding a free dataset for Türkiye, so I decided to focus my project on analyzing global, Europe, North America, and Japan sales, using a dataset I already had. I may explore Türkiye in the future, but for now, not having access to the right dataset could hinder my project's success. If companies face similar issues, they may need to create their own datasets for specific needs."
-paragraph2 = "If I had access to a Türkiye dataset, I planned to analyze the correlation between the country's economy and the popularity of certain video games globally, but not in Türkiye. For example, I wanted to explore why the Zelda series, popular globally, is not as popular in Türkiye due to limited Nintendo console users."
-paragraph3 = "The dataset I selected combines sales and scores, allowing me to conduct regionalized analyses, important for my project's goals. This saves me time from merging separate datasets, making it beneficial to my project."
+with open(file_path, "r") as file:
+    paragraphs = file.readlines()
 
-paragraph4 = "Firstly, we can see what the most popular games are for some regions that have global sales greater than 10 million. We need to consider that these sales are cumulative data until 2016. So, newer games have less sales. We will make our analysis with respect to this important information."
+paragraph_intro = paragraphs[0].strip()
+paragraph1 = paragraphs[1].strip()
+paragraph2 = paragraphs[2].strip()
+paragraph3 = paragraphs[3].strip()
+paragraph4 = paragraphs[4].strip()
+paragraph5 = paragraphs[5].strip()
+paragraph6 = paragraphs[6].strip()
+paragraph7 = paragraphs[7].strip()
+paragraph8 = paragraphs[8].strip()
+paragraph9 = paragraphs[9].strip()
+paragraph10 = paragraphs[10].strip()
 
-paragraph5 = "We can see that, except in Japan and other countries, Wii Sports is the most popular game. So, why is that? Let's take a look."
-paragraph6 = "In Japan, Pokémon has a massive following and has been a cultural phenomenon for over two decades. This popularity is likely due to the franchise's origins in Japan and its ability to appeal to a wide audience, including children and adults. In contrast, Wii Sports may not have had the same cultural relevance in Japan, despite its success in other regions."
-paragraph7 = "As for the Other countries (such as Middle East), Grand Theft Auto (GTA) is popular because of its open-world gameplay and its ability to allow players to experience a world that they may not be able to in real life. Additionally, the series' gritty and mature themes, which include crime and violence, may appeal to some audiences in the region."
-
-paragraph8 = "Firstly, it is evident that North America's sales generate the largest portion of sales in terms of numbers. This can be attributed to both the fact that the United States has one of the world's largest economies and the fact that the gaming culture has evolved extensively in North America. Additionally, this trend can also be observed in countries outside of the NA, EU, and Japan, where both sales numbers and economies are relatively lower."
-paragraph9 = "Secondly, it is apparent that the most popular genre in every region, except Japan, is action games. However, in Japan, RPGs (role-playing games) take the lead as the most popular genre. This is because Japanese gaming culture has developed around a strong affinity for role-playing games. Furthermore, unlike other countries, Japanese gamers generally have less interest in shooter games. Therefore, it can be concluded that if a video game company intends to target the Asian market, they should prioritize producing RPGs rather than shooters."
-paragraph10 = "Lastly, it is evident that globally, genres such as strategy, adventure, puzzle, simulation, and fighting are not as popular compared to action and RPG genres."
 
 # Render the header with an emoji
 st.title("The Use of Big Data for Understanding the Video Game Market 🎮📈")
@@ -104,7 +106,7 @@ with col3:
     st.write("\n")
     st.write(paragraph5)
     st.write(paragraph6)
-    st.write("Japan is embracing its own culture. In that case, a game can be developed based on newly popularized concepts within their culture.")
+    st.write("Japan is embracing its own culture and cultural products. In that case, a game can be developed based on newly popularized concepts within their culture.")
     st.write(paragraph7)
 
 st.header("Most Popular (Sold) Genres")
